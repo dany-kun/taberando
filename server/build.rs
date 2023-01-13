d69f1cf05f03ca5298cfbdc3e85d5d174c420576
daniel <dans44684@hotmail.com>
2023-01-13 19:14:00 +0900

    Fix build.rs

--- server/build.rs
+++ server/build.rs
@@ -1,15 +1,18 @@
-use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open(".env.json")?;
+fn main() {
+    let file = match File::open(".env.json") {
+        Ok(f) => f,
+        Err(_) => {
+            return;
+        }
+    };
     let reader = BufReader::new(file);
 
     // Read the JSON contents of the file as an instance of `User`.
-    let env_json: serde_json::Value = serde_json::from_reader(reader)?;
+    let env_json: serde_json::Value = serde_json::from_reader(reader).unwrap();
     for (key, value) in env_json.as_object().unwrap() {
         println!("cargo:rustc-env={}={}", key, value.as_str().unwrap());
     }
-    Ok(())
 }
